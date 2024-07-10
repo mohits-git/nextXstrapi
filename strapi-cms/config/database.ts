6cfb88d7c77008c579d73ef5fb4837dffec1078d
mohits-git <mohitkhatri2805@gmail.com>
2024-07-10 10:56:37 +0530

    feat: initialise with the working postgres strapi integration with ca cert. from envs

--- strapi-cms/config/database.ts
+++ strapi-cms/config/database.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from "fs";
 
 export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
@@ -55,16 +56,9 @@ export default ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
+        ssl: {
+          ca: env('DATABASE_SSL_CA', 'ca'),
+          rejectUnauthorized: true,
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
